81de13381e00c6ed67142de2c25e69014a422198
S83gamedev <156585260+S83gamedev@users.noreply.github.com>
2024-02-02 17:39:11 +0330

    Added Background squre and Shadow

--- addons/AudioSub/Custom_AudioStream.gd
+++ addons/AudioSub/Custom_AudioStream.gd
@@ -1,13 +1,12 @@
 @tool
 extends EditorPlugin
 
-#I understood these part thanks to cgpoint tutorial
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	add_custom_type("AudioStream_Sub", "AudioStreamPlayer", preload('res://addons/AudioSub/AudioStream_sub.gd'), preload('res://AudioSub_icon.svg'))
+	add_custom_type("AudioStream_Sub", "AudioStreamPlayer", preload('res://addons/AudioSub/AudioStream_sub.gd'), preload('res://addons/AudioSub/AudioSub_small_icon.svg'))
+	pass
+
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
 	remove_custom_type("AudioStream_Sub")
 	pass
